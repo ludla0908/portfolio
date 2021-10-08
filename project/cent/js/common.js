345893a231795ce2eb6232f1d085e36d9986064c
ludla0908 <91296730+ludla0908@users.noreply.github.com>
2021-10-08 16:04:58 +0900

    21.10.08

---
+++ project/cent/js/common.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+    //depth2
+    $('.depth2').hide();
+    $('.gnb >li').mouseenter(function(){
+        $(this).children('.depth2').stop().slideDown();
+    });
+    $('.gnb >li').mouseleave(function(){
+        $(this).children('.depth2').stop().slideUp();
+    });
+
+    //main slide
+    $('.ms').slick({
+        autoplay: true,
+        dots: true,    
+    });
+
+    //뉴스 슬라이드
+    $('.news_list').slick({
+        dots: false,
+        autoplay: true,
+        vertical: true,
+        arrows: false,
+      });
+
+    //gotop
+    $(window).scroll(function(){
+        if($(this).scrollTop()>100){
+            $('.gotop').fadeIn();
+        } else {
+            $('.gotop').fadeOut();
+        }
+    });
+    
+    $('.gotop').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 'slow');
+        return false;
+    });
+
+    //scroll trigger
+    $(window).scroll(function(){
+        $('.ani-stop').each(function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() * 0.3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).removeClass('ani-stop');
+             }
+            if (bottom_of_window < bottom_of_object) {
+                $(this).addClass('ani-stop');
+            }
+        });
+    });
+      
+});
